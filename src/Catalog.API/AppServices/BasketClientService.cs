684ad3d6b98aaf71d8ca8bfc88e2ed5bb1c9c054
Bao To <baotoq@outlook.com>
2020-02-18 00:28:34 +0700

    Up

---
+++ src/Catalog.API/AppServices/BasketClientService.cs
@@ -0,0 +1,16 @@
+﻿using Basket.API;
+
+namespace Catalog.API.AppServices
+{
+    public class BasketClientService
+    {
+        private readonly Basket.API.Basket.BasketClient _basketClient;
+
+        public BasketClientService(Basket.API.Basket.BasketClient basketClient)
+        {
+            _basketClient = basketClient;
+
+            _basketClient.SayHello(new HelloRequest());
+        }
+    }
+}
---
+++ src/Catalog.API/CatalogController.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Catalog.API.AppServices;
+using Microsoft.AspNetCore.Mvc;
+
+namespace Catalog.API
+{
+    [Route("api/catalogs")]
+    [ApiController]
+    public class CatalogController : ControllerBase
+    {
+        private BasketClientService _basketClientService;
+
+        public CatalogController(BasketClientService basketClientService)
+        {
+            _basketClientService = basketClientService;
+        }
+
+        [HttpGet]
+        public IActionResult Get()
+        {
+            return Ok();
+        }
+    }
+}
