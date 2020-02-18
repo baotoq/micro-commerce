39ffaa733d87fd618086dd4da968a5fd2c577563
Bao To <baotoq@outlook.com>
2020-02-18 18:14:55 +0700

    Up

--- src/Catalog.API/CatalogController.cs
+++ src/Catalog.API/CatalogController.cs
@@ -1,8 +1,4 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Catalog.API.AppServices;
+﻿using Catalog.API.AppServices;
 using Microsoft.AspNetCore.Mvc;
 
 namespace Catalog.API
