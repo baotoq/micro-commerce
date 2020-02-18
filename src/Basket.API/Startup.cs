85656ae46df50a0c349dc9d7462fb72037cbba83
Bao To Quoc <Bao.ToQuoc@nashtechglobal.com>
2020-02-18 18:14:55 +0700

    Up

--- src/Basket.API/Startup.cs
+++ src/Basket.API/Startup.cs
@@ -34,7 +34,13 @@ namespace Basket.API
             {
                 app.UseDeveloperExceptionPage();
             }
+            else
+            {
+                // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
+                app.UseHsts();
+            }
 
+            app.UseHttpsRedirection();
             app.UseRouting();
 
             app.UseEndpoints(endpoints =>
