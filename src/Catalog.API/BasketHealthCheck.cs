c0506a7b7f773ed050e02e09189de8d60a4c8178
Bao To Quoc <Bao.ToQuoc@nashtechglobal.com>
2020-02-26 17:38:51 +0700

    Up

---
+++ src/Catalog.API/BasketHealthCheck.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Threading;
+using System.Threading.Tasks;
+using Grpc.Health.V1;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Logging;
+using Microsoft.Extensions.Logging.Abstractions;
+
+namespace Catalog.API
+{
+    public class BasketHealthCheck : IHealthCheck
+    {
+        private readonly ILogger<BasketHealthCheck> _logger;
+        private readonly Health.HealthClient _healthClient;
+
+        public BasketHealthCheck(Health.HealthClient healthClient)
+        {
+            _logger = NullLogger<BasketHealthCheck>.Instance;
+            _healthClient = healthClient;
+        }
+
+        public async Task<HealthCheckResult> CheckHealthAsync(HealthCheckContext context, CancellationToken cancellationToken = new CancellationToken())
+        {
+            var health = await _healthClient.CheckAsync(new HealthCheckRequest());
+
+            _logger.LogInformation("Health Status: {Status}", health.Status);
+
+            return health.Status switch
+            {
+                HealthCheckResponse.Types.ServingStatus.Serving => HealthCheckResult.Healthy(),
+                _ => HealthCheckResult.Unhealthy()
+            };
+        }
+    }
+}
