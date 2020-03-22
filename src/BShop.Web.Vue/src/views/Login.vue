d3c370b28c6a6558782958fc986456a08fb910f2
Bao To <baotoq@outlook.com>
2020-03-22 23:49:39 +0700

    Up

---
+++ src/BShop.Web.Vue/src/views/Login.vue
@@ -0,0 +1,14 @@
+<template>
+  <div>Login</div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from "vue-property-decorator";
+
+@Component({
+  name: "Login"
+})
+export default class Login extends Vue {}
+</script>
+
+<style></style>
