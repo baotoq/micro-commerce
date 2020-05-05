993155a986d3d835efcc524b7c255de9cfe55941
To Quoc Bao <bao2703@gmail.com>
2020-05-05 21:26:42 +0700

    Up

--- src/BShop.API/Data/ApplicationDbContext.cs
+++ src/BShop.API/Data/ApplicationDbContext.cs
@@ -1,12 +1,35 @@
-﻿using Microsoft.EntityFrameworkCore;
+﻿using BShop.API.Data.Models;
+using Microsoft.EntityFrameworkCore;
 
 namespace BShop.API.Data
 {
     public class ApplicationDbContext : DbContext
     {
+        public DbSet<Product> Products { get; set; }
+
+        public DbSet<Category> Categories { get; set; }
+
         public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
             : base(options)
         {
+        }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            base.OnModelCreating(modelBuilder);
+
+            modelBuilder.Entity<ProductCategory>()
+                .HasKey(pc => new { pc.ProductId, pc.CategoryId });
+
+            modelBuilder.Entity<ProductCategory>()
+                .HasOne(pc => pc.Product)
+                .WithMany(p => p.ProductCategories)
+                .HasForeignKey(pc => pc.ProductId);
+
+            modelBuilder.Entity<ProductCategory>()
+                .HasOne(pc => pc.Category)
+                .WithMany(c => c.ProductCategories)
+                .HasForeignKey(pc => pc.CategoryId);
         }
     }
 }
