8992f011fe3da696bddbb6be32cb867658e6caba
Bao To <baotoq@outlook.com>
2020-05-05 21:26:42 +0700

    Up

---
+++ src/BShop.API/Data/Models/Category.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BShop.API.Data.Models
+{
+    public class Category
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IList<ProductCategory> ProductCategories { get; private set; } = new List<ProductCategory>();
+    }
+}
