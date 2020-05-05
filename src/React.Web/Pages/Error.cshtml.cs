b30673ecb3fe682038e1f2ee4909b37bc26ba3c7
To Quoc Bao <bao2703@gmail.com>
2020-05-05 23:07:11 +0700

    Add react

---
+++ src/React.Web/Pages/Error.cshtml.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.RazorPages;
+using Microsoft.Extensions.Logging;
+
+namespace React.Web.Pages
+{
+    [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
+    public class ErrorModel : PageModel
+    {
+        private readonly ILogger<ErrorModel> logger;
+
+        public ErrorModel(ILogger<ErrorModel> _logger)
+        {
+            logger = _logger;
+        }
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+
+        public void OnGet()
+        {
+            RequestId = Activity.Current?.Id ?? HttpContext.TraceIdentifier;
+        }
+    }
+}
