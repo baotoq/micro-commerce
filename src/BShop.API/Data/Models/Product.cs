14bdc4cd046d6875560875a403056f63340f00c9
To Quoc Bao <bao2703@gmail.com>
2020-05-05 21:26:42 +0700

    Up

---
+++ src/BShop.API/Data/Models/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace BShop.API.Data.Models
+{
+    public class Product
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageFileName { get; set; }
+
+        public int BrandId { get; set; }
+
+        public IList<ProductCategory> ProductCategories { get; private set; } = new List<ProductCategory>();
+    }
+}
--- src/Identity.API/Configurations/IdentityServerConfig.cs
+++ src/Identity.API/Configurations/IdentityServerConfig.cs
@@ -1,4 +1,6 @@
 using System.Collections.Generic;
+using System.Runtime.InteropServices.ComTypes;
+using IdentityServer4;
 using IdentityServer4.Models;
 
 namespace Identity.API.Configurations
@@ -15,7 +17,7 @@ namespace Identity.API.Configurations
         public static IEnumerable<ApiResource> Apis =>
             new[]
             {
-                new ApiResource("bshop", "BShop API")
+                new ApiResource("bshop-api", "BShop API")
             };
 
         public static IEnumerable<Client> Clients =>
@@ -23,10 +25,45 @@ namespace Identity.API.Configurations
             {
                 new Client
                 {
-                    ClientId = "bshop.ro.client",
+                    ClientId = "bshop.ro",
                     AllowedGrantTypes = GrantTypes.ResourceOwnerPassword,
                     ClientSecrets = { new Secret("secret".Sha256()) },
                     AllowedScopes = { "bshop-api" }
+                },
+                new Client
+                {
+                    ClientName = "react_code_client",
+                    ClientId = "react_code_client",
+                    AccessTokenType = AccessTokenType.Reference,
+                    AllowedGrantTypes = GrantTypes.Code,
+                    AllowAccessTokensViaBrowser = true,
+
+                    RequireClientSecret = false,
+                    RequireConsent = false,
+                    RequirePkce = true,
+
+                    RedirectUris = new List<string>
+                    {
+                        $"http://localhost:3000/authentication/login-callback",
+                        $"http://localhost:3000/silent-renew.html",
+                        $"http://localhost:3000"
+                    },
+                    PostLogoutRedirectUris = new List<string>
+                    {
+                        $"http://localhost:3000/unauthorized",
+                        $"http://localhost:3000/authentication/logout-callback",
+                        $"http://localhost:3000"
+                    },
+                    AllowedCorsOrigins = new List<string>
+                    {
+                        $"http://localhost:3000"
+                    },
+                    AllowedScopes = new List<string>
+                    {
+                        IdentityServerConstants.StandardScopes.OpenId,
+                        IdentityServerConstants.StandardScopes.Profile,
+                        "bshop-api"
+                    }
                 }
             };
     }
