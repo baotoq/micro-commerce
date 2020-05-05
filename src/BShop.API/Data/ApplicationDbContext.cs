9ad42131dd8eb7f52e516901820e00df072380e1
Bao To <baotoq@outlook.com>
2020-05-05 17:52:25 +0700

    Up

---
+++ src/BShop.API/Data/ApplicationDbContext.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.EntityFrameworkCore;
+
+namespace BShop.API.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options)
+            : base(options)
+        {
+        }
+    }
+}
