dae3e724d1c76ace0ab9a6cdf43100fcf1341f38
To Quoc Bao <bao2703@gmail.com>
2020-05-05 21:52:05 +0700

    Up

---
+++ src/BShop.API/Controllers/ViewModels/CategoryViewModel.cs
@@ -0,0 +1,9 @@
+﻿namespace BShop.API.Controllers.ViewModels
+{
+    public class CategoryViewModel
+    {
+        public int Id { get; set; }
+
+        public string Name { get; set; }
+    }
+}
--- src/Identity.API/Program.cs
+++ src/Identity.API/Program.cs
@@ -1,5 +1,6 @@
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Hosting;
+using Serilog;
 
 namespace Identity.API
 {
@@ -12,6 +13,7 @@ namespace Identity.API
 
         public static IHostBuilder CreateHostBuilder(string[] args) =>
             Host.CreateDefaultBuilder(args)
+                .UseSerilog((hostingContext, loggerConfiguration) => loggerConfiguration.ReadFrom.Configuration(hostingContext.Configuration))
                 .ConfigureWebHostDefaults(webBuilder =>
                 {
                     webBuilder.UseStartup<Startup>();
