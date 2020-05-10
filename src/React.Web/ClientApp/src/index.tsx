b3bcd6e0205898187844c4359174c2485582f2c2
To Quoc Bao <bao2703@gmail.com>
2020-05-11 02:20:16 +0700

    Feature/add auth react (#4)
    * Add authentication
    
    * add package-lock.json

--- src/React.Web/ClientApp/src/index.tsx
+++ src/React.Web/ClientApp/src/index.tsx
@@ -1,27 +1,24 @@
+import React from "react";
+import ReactDOM from "react-dom";
+import * as serviceWorker from "./serviceWorker";
+
 import "bootstrap/dist/css/bootstrap.css";
 
-import * as React from "react";
-import * as ReactDOM from "react-dom";
+import App from "./containers/App";
+
+import { store } from "./store/store";
 import { Provider } from "react-redux";
-import { ConnectedRouter } from "connected-react-router";
-import { createBrowserHistory } from "history";
-import configureStore from "./store/configureStore";
-import App from "./App";
-import registerServiceWorker from "./registerServiceWorker";
-
-// Create browser history to use in the Redux store
-const baseUrl = document.getElementsByTagName("base")[0].getAttribute("href") as string;
-const history = createBrowserHistory({ basename: baseUrl });
-
-// Get the application-wide store instance, prepopulating with state from the server where available.
-const store = configureStore(history);
 
 ReactDOM.render(
+  <React.StrictMode>
     <Provider store={store}>
-        <ConnectedRouter history={history}>
-            <App />
-        </ConnectedRouter>
-    </Provider>,
-    document.getElementById("root"));
+      <App />
+    </Provider>
+  </React.StrictMode>,
+  document.getElementById("root")
+);
 
-registerServiceWorker();
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
