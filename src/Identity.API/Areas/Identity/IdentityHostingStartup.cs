32674597cc31b37b8a3ccd1a6e72aa3a84c74120
To Quoc Bao <bao2703@gmail.com>
2020-05-12 02:07:13 +0700

    Fix logout

---
+++ src/Identity.API/Areas/Identity/IdentityHostingStartup.cs
@@ -0,0 +1,13 @@
+﻿using Microsoft.AspNetCore.Hosting;
+
+[assembly: HostingStartup(typeof(Identity.API.Areas.Identity.IdentityHostingStartup))]
+namespace Identity.API.Areas.Identity
+{
+    public class IdentityHostingStartup : IHostingStartup
+    {
+        public void Configure(IWebHostBuilder builder)
+        {
+            builder.ConfigureServices((context, services) => { });
+        }
+    }
+}
