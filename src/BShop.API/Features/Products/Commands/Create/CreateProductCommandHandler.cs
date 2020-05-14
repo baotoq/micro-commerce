d39819b10d11c0ebfa5c5ac1e9d5ce960cf24f01
To Quoc Bao <bao2703@gmail.com>
2020-05-15 02:37:46 +0700

    Add products

---
+++ src/BShop.API/Features/Products/Commands/Create/CreateProductCommandHandler.cs
@@ -0,0 +1,45 @@
+﻿using MediatR;
+using System.Threading;
+using System.Threading.Tasks;
+using BShop.API.Data;
+using BShop.API.Data.Models;
+using BShop.API.Features.Products.Models;
+
+namespace BShop.API.Features.Products.Commands.Create
+{
+    public class CreateProductCommandHandler : IRequestHandler<CreateProductCommand, ProductDto>
+    {
+        private readonly IRepository<Product> _repository;
+
+        public CreateProductCommandHandler(IRepository<Product> repository)
+        {
+            _repository = repository;
+        }
+
+        public async Task<ProductDto> Handle(CreateProductCommand request, CancellationToken cancellationToken)
+        {
+            var product = new Product
+            {
+                Name = request.Name
+            };
+
+            foreach (var categoryId in request.CategoryIds)
+            {
+                var productCategory = new ProductCategory
+                {
+                    CategoryId = categoryId
+                };
+                product.ProductCategories.Add(productCategory);
+            }
+
+            await _repository.AddAsync(product, cancellationToken);
+            await _repository.SaveChangesAsync(cancellationToken);
+
+            return new ProductDto
+            {
+                Id = product.Id,
+                Name = product.Name
+            };
+        }
+    }
+}
---
+++ src/BShop.API/Features/Products/Models/ProductDto.cs
@@ -0,0 +1,13 @@
+﻿using System.Collections.Generic;
+
+namespace BShop.API.Features.Products.Models
+{
+    public class ProductDto
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public IList<ProductCategoryDto> Categories { get; set; }
+    }
+}
