c61d6cf6f71d6ff1a0f405d56a20fdb0be9638b7
To Quoc Bao <bao2703@gmail.com>
2020-05-18 16:06:33 +0700

    Up

---
+++ tests/FunctionalTests/BShop.API.FunctionalTests/Infrastructure/TestWebApplicationFactoryExtension.cs
@@ -0,0 +1,43 @@
+﻿using System.Net.Http;
+using System.Net.Http.Headers;
+using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Authentication.JwtBearer;
+using Microsoft.AspNetCore.Mvc.Testing;
+using Microsoft.AspNetCore.TestHost;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace BShop.API.FunctionalTests.Infrastructure
+{
+    public static class TestWebApplicationFactoryExtension
+    {
+        public static HttpClient CreateAuthenticatedClient<TStartup>(this TestWebApplicationFactory<TStartup> factory) where TStartup : class
+        {
+            var client = factory.WithWebHostBuilder(builder =>
+                {
+                    builder.ConfigureTestServices(services =>
+                    {
+                        services.AddAuthentication("Test")
+                            .AddScheme<AuthenticationSchemeOptions, TestAuthenticationHandler>(
+                                "Test", options => { });
+
+                        services.AddAuthorization(options =>
+                        {
+                            options.AddPolicy(JwtBearerDefaults.AuthenticationScheme, policy =>
+                            {
+                                policy.AddAuthenticationSchemes("Test");
+                                policy.RequireAuthenticatedUser();
+                            });
+                        });
+                    });
+                })
+                .CreateClient(new WebApplicationFactoryClientOptions
+                {
+                    AllowAutoRedirect = false,
+                });
+
+            client.DefaultRequestHeaders.Authorization = new AuthenticationHeaderValue("Test");
+
+            return client;
+        }
+    }
+}
