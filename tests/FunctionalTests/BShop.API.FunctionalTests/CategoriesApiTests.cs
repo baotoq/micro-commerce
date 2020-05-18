1f2b459f7c71f8b0e095ad464d4984f96670a7b7
Bao To <baotoq@outlook.com>
2020-05-18 16:06:33 +0700

    Up

---
+++ tests/FunctionalTests/BShop.API.FunctionalTests/CategoriesApiTests.cs
@@ -0,0 +1,86 @@
+﻿using System.Collections.Generic;
+using System.Net;
+using System.Net.Http;
+using System.Threading.Tasks;
+using BShop.API.Features.Categories.Commands.Create;
+using BShop.API.Features.Categories.Models;
+using BShop.API.FunctionalTests.Infrastructure;
+using FluentAssertions;
+using Xunit;
+
+namespace BShop.API.FunctionalTests
+{
+    public class CategoriesApiTests : IClassFixture<TestWebApplicationFactory<Startup>>
+    {
+        private readonly TestWebApplicationFactory<Startup> _factory;
+
+        public CategoriesApiTests(TestWebApplicationFactory<Startup> factory)
+        {
+            _factory = factory;
+        }
+
+        [Fact]
+        public async Task GetAll_Success()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("api/categories");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var result = await response.Content.ReadAsAsync<IEnumerable<CategoryDto>>();
+            result.Should().NotBeNullOrEmpty();
+        }
+
+        [Fact]
+        public async Task Get_Success()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+
+            // Act
+            var response = await client.GetAsync("api/categories/1");
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.OK);
+            var result = await response.Content.ReadAsAsync<CategoryDto>();
+            result.Should().NotBeNull();
+        }
+
+        [Fact]
+        public async Task Create_NoAuthentication_Unauthorized()
+        {
+            // Arrange
+            var client = _factory.CreateClient();
+            var category = new CreateCategoryCommand { Name = "Test no authentication" };
+
+            // Act
+            var response = await client.PostAsJsonAsync("api/categories", category);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Unauthorized);
+        }
+
+        [Fact]
+        public async Task Create_Authenticated_Success()
+        {
+            // Arrange
+            var client = _factory.CreateAuthenticatedClient();
+            var category = new CreateCategoryCommand { Name = "Test authenticated" };
+
+            // Act
+            var response = await client.PostAsJsonAsync("api/categories", category);
+
+            // Assert
+            response.StatusCode.Should().Be(HttpStatusCode.Created);
+            var result = await response.Content.ReadAsAsync<CategoryDto>();
+            result.Should().BeEquivalentTo(new CategoryDto
+            {
+                Id = 2,
+                Name = "Test authenticated"
+            });
+        }
+    }
+}
