a0ea7e0ecd5c49782af15d369d4a4709c889d708
To Quoc Bao <bao2703@gmail.com>
2020-09-04 17:31:35 +0700

    Update README.md

--- src/Catalog.API/ApiControllers/CategoriesController.cs
+++ src/Catalog.API/ApiControllers/CategoriesController.cs
@@ -1,5 +1,4 @@
-﻿using System;
-using System.Threading;
+﻿using System.Threading;
 using System.Threading.Tasks;
 using Catalog.API.Application.Categories.Commands;
 using Catalog.API.Application.Categories.Models;
--- src/Ordering.API/ApiControllers/SeedController.cs
+++ src/Ordering.API/ApiControllers/SeedController.cs
@@ -1,5 +1,4 @@
-﻿using System.Collections.Generic;
-using System.Threading.Tasks;
+﻿using System.Threading.Tasks;
 using System.Transactions;
 using Dapper;
 using Data.UnitOfWork;
