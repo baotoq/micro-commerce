a6c5d9105e08ec8d63ecf7ace8c3a90abc085656
To Quoc Bao <bao2703@gmail.com>
2020-09-09 02:18:05 +0700

    Up

--- src/react-web/src/index.js
+++ src/react-web/src/index.js
@@ -1,10 +1,11 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
-import App from "./App";
-import store from "./app/store";
 import { Provider } from "react-redux";
 import * as serviceWorker from "./serviceWorker";
+
+import "./index.css";
+import App from "./features/app/App";
+import store from "./store/store";
 
 ReactDOM.render(
   <React.StrictMode>
