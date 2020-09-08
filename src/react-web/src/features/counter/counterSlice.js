52255cedf22a05cc064b66f4ccd1cf6df8c9f305
To Quoc Bao <bao2703@gmail.com>
2020-09-09 02:18:05 +0700

    Up

--- src/react-web/src/features/counter/counterSlice.js
+++ src/react-web/src/features/counter/counterSlice.js
@@ -1,4 +1,6 @@
 import { createSlice } from "@reduxjs/toolkit";
+import { delay, map } from "rxjs/operators";
+import { of } from "rxjs";
 
 export const counterSlice = createSlice({
   name: "counter",
@@ -7,10 +9,6 @@ export const counterSlice = createSlice({
   },
   reducers: {
     increment: (state) => {
-      // Redux Toolkit allows us to write "mutating" logic in reducers. It
-      // doesn't actually mutate the state because it uses the Immer library,
-      // which detects changes to a "draft state" and produces a brand new
-      // immutable state based off those changes
       state.value += 1;
     },
     decrement: (state) => {
@@ -19,24 +17,31 @@ export const counterSlice = createSlice({
     incrementByAmount: (state, action) => {
       state.value += action.payload;
     },
+    incrementAsync: () => {},
   },
 });
 
-export const { increment, decrement, incrementByAmount } = counterSlice.actions;
+export const {
+  increment,
+  decrement,
+  incrementByAmount,
+  incrementAsync,
+} = counterSlice.actions;
 
-// The function below is called a thunk and allows us to perform async logic. It
-// can be dispatched like a regular action: `dispatch(incrementAsync(10))`. This
-// will call the thunk with the `dispatch` function as the first argument. Async
-// code can then be executed and other actions can be dispatched
-export const incrementAsync = (amount) => (dispatch) => {
-  setTimeout(() => {
-    dispatch(incrementByAmount(amount));
-  }, 1000);
-};
+// export const incrementAsync = (amount) => (dispatch) => {
+//   setTimeout(() => {
+//     dispatch(incrementByAmount(amount));
+//   }, 1000);
+// };
 
-// The function below is called a selector and allows us to select a value from
-// the state. Selectors can also be defined inline where they're used instead of
-// in the slice file. For example: `useSelector((state) => state.counter.value)`
 export const selectCount = (state) => state.counter.value;
 
 export default counterSlice.reducer;
+
+export const epics = {
+  incrementAsync: (action$, state$, action) =>
+    of([]).pipe(
+      delay(1000),
+      map(() => incrementByAmount(2))
+    ),
+};
