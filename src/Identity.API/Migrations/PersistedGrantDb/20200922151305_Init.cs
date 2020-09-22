aa2f9c338ae56428407cc80d384bb27c830f9a9b
Bao To <baotoq@outlook.com>
2020-09-22 22:30:25 +0700

    Up

---
+++ src/Identity.API/Migrations/PersistedGrantDb/20200922151305_Init.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace Identity.API.Migrations.PersistedGrantDb
+{
+    public partial class Init : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "DeviceCodes",
+                columns: table => new
+                {
+                    user_code = table.Column<string>(maxLength: 200, nullable: false),
+                    device_code = table.Column<string>(maxLength: 200, nullable: false),
+                    subject_id = table.Column<string>(maxLength: 200, nullable: true),
+                    client_id = table.Column<string>(maxLength: 200, nullable: false),
+                    creation_time = table.Column<DateTime>(nullable: false),
+                    expiration = table.Column<DateTime>(nullable: false),
+                    data = table.Column<string>(maxLength: 50000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_device_codes", x => x.user_code);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "PersistedGrants",
+                columns: table => new
+                {
+                    key = table.Column<string>(maxLength: 200, nullable: false),
+                    type = table.Column<string>(maxLength: 50, nullable: false),
+                    subject_id = table.Column<string>(maxLength: 200, nullable: true),
+                    client_id = table.Column<string>(maxLength: 200, nullable: false),
+                    creation_time = table.Column<DateTime>(nullable: false),
+                    expiration = table.Column<DateTime>(nullable: true),
+                    data = table.Column<string>(maxLength: 50000, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("pk_persisted_grants", x => x.key);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "ix_device_codes_device_code",
+                table: "DeviceCodes",
+                column: "device_code",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "ix_device_codes_expiration",
+                table: "DeviceCodes",
+                column: "expiration");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_persisted_grants_expiration",
+                table: "PersistedGrants",
+                column: "expiration");
+
+            migrationBuilder.CreateIndex(
+                name: "ix_persisted_grants_subject_id_client_id_type",
+                table: "PersistedGrants",
+                columns: new[] { "subject_id", "client_id", "type" });
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "DeviceCodes");
+
+            migrationBuilder.DropTable(
+                name: "PersistedGrants");
+        }
+    }
+}
