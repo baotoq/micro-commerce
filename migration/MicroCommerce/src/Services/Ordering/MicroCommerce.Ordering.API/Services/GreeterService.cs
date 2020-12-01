c83c083b16ca3d6fd24e3387154382d4d206d848
To Quoc Bao <bao2703@gmail.com>
2020-12-01 15:16:42 +0700

    Up

---
+++ migration/MicroCommerce/src/Services/Ordering/MicroCommerce.Ordering.API/Services/GreeterService.cs
@@ -0,0 +1,24 @@
+﻿using System.Threading.Tasks;
+using Grpc.Core;
+using Microsoft.Extensions.Logging;
+
+namespace MicroCommerce.Ordering.API.Services
+{
+    public class GreeterService : Greeter.GreeterBase
+    {
+        private readonly ILogger<GreeterService> _logger;
+
+        public GreeterService(ILogger<GreeterService> logger)
+        {
+            _logger = logger;
+        }
+
+        public override Task<HelloReply> SayHello(HelloRequest request, ServerCallContext context)
+        {
+            return Task.FromResult(new HelloReply
+            {
+                Message = "Hello " + request.Name
+            });
+        }
+    }
+}
