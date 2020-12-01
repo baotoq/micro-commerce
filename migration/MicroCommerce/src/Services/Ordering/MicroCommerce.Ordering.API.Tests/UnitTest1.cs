28a2f59a26fb120352aefe43cce6c642124c738f
To Quoc Bao <bao2703@gmail.com>
2020-12-01 15:21:15 +0700

    Up

---
+++ migration/MicroCommerce/src/Services/Ordering/MicroCommerce.Ordering.API.Tests/UnitTest1.cs
@@ -0,0 +1,13 @@
+﻿using Xunit;
+
+namespace MicroCommerce.Ordering.API.Tests
+{
+    public class UnitTest1
+    {
+        [Fact]
+        public void Test1()
+        {
+            Assert.True(true);
+        }
+    }
+}
