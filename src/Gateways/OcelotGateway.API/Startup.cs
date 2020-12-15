fd891ddaffa142bde6ff9a38834cebc7045d3a7f
To Quoc Bao <bao2703@gmail.com>
2020-12-15 22:16:19 +0700

    Add api gateway

---
+++ src/Gateways/OcelotGateway.API/Startup.cs
@@ -0,0 +1,93 @@
+﻿using System;
+using System.Reflection;
+using Microsoft.AspNetCore.Builder;
+using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Microsoft.Extensions.Hosting;
+using Ocelot.DependencyInjection;
+using Ocelot.Middleware;
+using Ocelot.Provider.Consul;
+using OpenTelemetry.Trace;
+using Prometheus;
+using Serilog;
+
+namespace OcelotGateway.API
+{
+    public class Startup
+    {
+        public Startup(IConfiguration configuration)
+        {
+            Configuration = configuration;
+        }
+
+        public IConfiguration Configuration { get; }
+
+        // This method gets called by the runtime. Use this method to add services to the container.
+        // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddCors();
+            services.AddHealthChecks().ForwardToPrometheus();
+
+            services.AddOcelot()
+                .AddConsul()
+                .AddAdministration("/admin");
+
+            //services.AddAuthentication()
+                //.AddJwtBearer("Bearer", options =>
+                //{
+                //    options.Authority = "https://whereyouridentityserverlives.com";
+                //});
+
+            services.AddOpenTelemetryTracing(builder =>
+            {
+                builder
+                    .AddAspNetCoreInstrumentation()
+                    .AddHttpClientInstrumentation()
+                    .SetSampler(new AlwaysOnSampler())
+                    .AddZipkinExporter(option =>
+                    {
+                        option.ServiceName = Assembly.GetExecutingAssembly().GetName().Name;
+                        option.Endpoint = new Uri(Configuration["OpenTelemetry:ZipkinEndpoint"]);
+                    });
+            });
+        }
+
+        // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
+        public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
+        {
+            if (env.IsDevelopment())
+            {
+                app.UseDeveloperExceptionPage();
+            }
+
+            app.UseSerilogRequestLogging();
+
+            app.UseCors(builder =>
+            {
+                builder
+                    .AllowAnyOrigin()
+                    .AllowAnyHeader()
+                    .AllowAnyMethod();
+            });
+
+            app.UseRouting();
+
+            app.UseHttpMetrics();
+
+            app.UseAuthorization();
+
+            app.UseEndpoints(endpoints =>
+            {
+                endpoints.MapGet("/", async context =>
+                {
+                    await context.Response.WriteAsync("Ocelot gateway hello!");
+                });
+            });
+
+            app.UseOcelot().Wait();
+        }
+    }
+}
