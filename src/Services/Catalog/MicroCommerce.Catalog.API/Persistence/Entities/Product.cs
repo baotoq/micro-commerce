69734850bceefb3a54577b9a8d9ad0d9dfe70596
To Quoc Bao <bao2703@gmail.com>
2020-12-30 22:38:04 +0700

    Up

---
+++ src/Services/Catalog/MicroCommerce.Catalog.API/Persistence/Entities/Product.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections.Generic;
+
+namespace MicroCommerce.Catalog.API.Persistence.Entities
+{
+    public class Product
+    {
+        public long Id { get; set; }
+
+        public string Name { get; set; }
+
+        public decimal Price { get; set; }
+
+        public int StockQuantity { get; set; }
+
+        public string Description { get; set; }
+
+        public string ImageUri { get; set; }
+
+        public ICollection<Category> Categories { get; protected set; }
+    }
+}
