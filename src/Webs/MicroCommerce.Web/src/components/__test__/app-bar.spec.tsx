207e53f5f176b83988fd72aee512c90f657d1f76
Bao To <baotoq@outlook.com>
2021-01-09 23:36:29 +0700

    Add jest

--- src/Webs/MicroCommerce.Web/src/components/__test__/app-bar.spec.tsx
+++ src/Webs/MicroCommerce.Web/src/components/__test__/app-bar.spec.tsx
@@ -2,17 +2,17 @@ import React from "react";
 import renderer from "react-test-renderer";
 import { render, screen } from "@testing-library/react";
 
-import Footer from "../footer";
+import AppBar from "../app-bar";
 
-describe("Footer", () => {
+describe("AppBar", () => {
   it("should match snapshot", () => {
-    const component = renderer.create(<Footer />);
+    const component = renderer.create(<AppBar />);
     const tree = component.toJSON();
     expect(tree).toMatchSnapshot();
   });
 
   it("should show app name", () => {
-    render(<Footer />);
+    render(<AppBar />);
     expect(screen.getByText("Micro Commerce")).toBeInTheDocument();
   });
 });
