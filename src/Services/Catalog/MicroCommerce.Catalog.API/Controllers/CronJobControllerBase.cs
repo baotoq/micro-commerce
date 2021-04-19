463799e21aebf3095e7b0919e3ed016ad3d29d6a
Bao To <baotoq@outlook.com>
2021-04-19 14:11:34 +0700

    Add smtp binding, papercut

--- src/Services/Catalog/MicroCommerce.Catalog.API/Controllers/CronJobControllerBase.cs
+++ src/Services/Catalog/MicroCommerce.Catalog.API/Controllers/CronJobControllerBase.cs
@@ -1,5 +1,7 @@
 ﻿using System;
+using System.Collections.Generic;
 using System.Threading.Tasks;
+using Dapr.Client;
 using MediatR;
 using MicroCommerce.Catalog.API.Infrastructure;
 using Microsoft.AspNetCore.Mvc;
@@ -9,15 +11,34 @@ namespace MicroCommerce.Catalog.API.Controllers
 {
     public class CronJobController : ApiControllerBase
     {
-        public CronJobController(ILogger<CronJobController> logger, IMediator mediator) : base(logger, mediator)
+        private readonly DaprClient _daprClient;
+
+        public CronJobController(ILogger<CronJobController> logger, IMediator mediator, DaprClient daprClient) : base(logger, mediator)
         {
+            _daprClient = daprClient;
         }
 
         [HttpPost("cron-test")]
-        public Task Handle()
+        public async Task<IActionResult> Handle()
         {
+            var metadata = new Dictionary<string, string>
+            {
+                {"emailFrom", "eShopOn@dapr.io"},
+                {"emailTo", "dapr-smtp-binding@example.net"},
+                {"subject", $"Your eShopOnDapr order"}
+            };
+            try
+            {
+                await _daprClient.InvokeBindingAsync("smtp", "create", "Testing Dapr SMTP Binding", metadata);
+            }
+            catch (Exception e)
+            {
+                Console.WriteLine(e);
+                throw;
+            }
             Logger.LogInformation($"Cron start @{DateTime.Now}");
-            return Task.CompletedTask;
+
+            return Ok();
         }
     }
 }
