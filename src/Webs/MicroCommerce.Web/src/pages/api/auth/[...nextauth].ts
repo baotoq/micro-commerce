454e2d08dd7040b614d84d590a2c70199a4eb37b
Bao To <baotoq@outlook.com>
2021-06-19 02:02:10 +0700

    Update config

--- src/Webs/MicroCommerce.Web/src/pages/api/auth/[...nextauth].ts
+++ src/Webs/MicroCommerce.Web/src/pages/api/auth/[...nextauth].ts
@@ -6,7 +6,7 @@ export default NextAuth({
     Providers.IdentityServer4({
       id: "identity-server4",
       name: "IdentityServer4",
-      scope: "openid profile catalog.api",
+      scope: "openid profile catalog-api",
       domain: "localhost:15001",
       clientId: "react-web",
       clientSecret: "secret",
