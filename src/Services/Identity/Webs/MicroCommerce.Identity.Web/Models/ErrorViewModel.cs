9e6fea36bab5f2673929504d68de4c7213e9e2fc
Bao To <baotoq@outlook.com>
2021-07-26 14:46:53 +0700

    Added identity web skeletons

---
+++ src/Services/Identity/Webs/MicroCommerce.Identity.Web/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace MicroCommerce.Identity.Web.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
