ca673a07923528efc534ac1794fb91c2a8c7bf2e
Bao To <baotoq@outlook.com>
2023-08-26 16:04:43 +0700

    up

--- MicroCommerce/src/ProductService/WeatherForecast.cs
+++ MicroCommerce/src/ProductService/WeatherForecast.cs
@@ -1,4 +1,4 @@
-namespace CartService;
+namespace ProductService;
 
 public class WeatherForecast
 {
@@ -6,7 +6,7 @@ public class WeatherForecast
 
     public int TemperatureC { get; set; }
 
-    public int TemperatureF => 32 + (int) (TemperatureC / 0.5556);
+    public int TemperatureF => 32 + (int)(TemperatureC / 0.5556);
 
     public string? Summary { get; set; }
 }
