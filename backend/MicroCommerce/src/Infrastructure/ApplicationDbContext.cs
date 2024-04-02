5b1164188f9745d61ffe5128c365d267f958eb97
Bao To <baotoq@outlook.com>
2024-04-03 01:10:06 +0700

    up

--- backend/MicroCommerce/src/Infrastructure/ApplicationDbContext.cs
+++ backend/MicroCommerce/src/Infrastructure/ApplicationDbContext.cs
@@ -14,6 +14,10 @@ public class ApplicationDbContext : IdentityDbContext<User>
     {
     }
     
+    public DbSet<Buyer> Buyers { get; set; } = null!;
     public DbSet<Cart> Carts { get; set; } = null!;
+    public DbSet<DeliveryOption> DeliveryOptions { get; set; } = null!;
     public DbSet<Product> Products { get; set; } = null!;
+    public DbSet<Promotion> Promotions { get; set; } = null!;
+    public DbSet<Seller> Sellers { get; set; } = null!;
 }
