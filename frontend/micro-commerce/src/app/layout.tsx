144b81d479c958607ed483d46538554235d46f84
Bao To <baotoq@outlook.com>
2024-04-02 23:35:00 +0700

    up

--- frontend/micro-commerce/src/app/layout.tsx
+++ frontend/micro-commerce/src/app/layout.tsx
@@ -1,13 +1,19 @@
 import type { Metadata } from "next";
-import { Inter } from "next/font/google";
-
-import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
-import Button from "@mui/material/Button";
+import Link from "next/link";
 
 import "./globals.css";
-import NavLink from "@/components/nav-link";
 
+import { Inter } from "next/font/google";
 const inter = Inter({ subsets: ["latin"] });
+
+import { AppRouterCacheProvider } from "@mui/material-nextjs/v13-appRouter";
+import AppBar from "@mui/material/AppBar";
+import Box from "@mui/material/Box";
+import Toolbar from "@mui/material/Toolbar";
+import Typography from "@mui/material/Typography";
+import Button from "@mui/material/Button";
+import IconButton from "@mui/material/IconButton";
+import MenuIcon from "@mui/icons-material/Menu";
 
 export const metadata: Metadata = {
   title: "Micro Commerce",
@@ -22,7 +28,35 @@ export default function RootLayout({
   return (
     <html lang="en">
       <body className={inter.className}>
-        <Button variant="text">Text</Button>
+        <Box sx={{ flexGrow: 1 }}>
+          <AppBar position="static">
+            <Toolbar>
+              <IconButton
+                size="large"
+                edge="start"
+                color="inherit"
+                aria-label="menu"
+                sx={{ mr: 2 }}
+              >
+                <MenuIcon />
+              </IconButton>
+              <Typography
+                variant="h6"
+                component={Link}
+                href="/"
+                sx={{ flexGrow: 1 }}
+              >
+                My Commerce
+              </Typography>
+              <Button component={Link} color="inherit" href="/login">
+                Login
+              </Button>
+              <Button component={Link} color="inherit" href="/register">
+                Register
+              </Button>
+            </Toolbar>
+          </AppBar>
+        </Box>
         <AppRouterCacheProvider>{children}</AppRouterCacheProvider>
       </body>
     </html>
--- frontend/micro-commerce/src/components/nav-link.tsx
+++
@@ -1,5 +0,0 @@
-import Link from "next/link";
-
-export default function NavLink({ href, children }: any) {
-  return <Link href={href}>{children}</Link>;
-}
