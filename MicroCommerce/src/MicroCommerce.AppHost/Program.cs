4497827117304cef004789590d8c37cab3d1c773
Bao To <baotoq@outlook.com>
2024-04-14 13:03:00 +0700

    migrating to aspire

---
+++ MicroCommerce/src/MicroCommerce.AppHost/Program.cs
@@ -0,0 +1,11 @@
+var builder = DistributedApplication.CreateBuilder(args);
+
+var cache = builder.AddRedis("cache");
+
+var apiService = builder.AddProject<Projects.MicroCommerce_ApiService>("apiservice");
+
+builder.AddProject<Projects.MicroCommerce_Web>("webfrontend")
+    .WithReference(cache)
+    .WithReference(apiService);
+
+builder.Build().Run();
