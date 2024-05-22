c2471333a0764beb688b35ec7285d81919ff1595
Bao To <baotoq@outlook.com>
2024-05-22 22:23:21 +0700

    add test container

--- code/tests/MicroCommerce.ApiService.UnitTests/TestBase.cs
+++ code/tests/MicroCommerce.ApiService.UnitTests/TestBase.cs
@@ -1,23 +1,32 @@
 using MicroCommerce.ApiService.Infrastructure;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Diagnostics;
+using Testcontainers.PostgreSql;
 
 namespace MicroCommerce.ApiService.UnitTests;
 
-public abstract class TestBase
+public abstract class TestBase : IAsyncLifetime
 {
-    protected readonly ApplicationDbContext Context;
-
-    protected TestBase()
+    protected ApplicationDbContext Context;
+    private readonly PostgreSqlContainer _postgres = new PostgreSqlBuilder()
+        .Build();
+    
+    public async Task InitializeAsync()
     {
+        await _postgres.StartAsync();
+        
         var contextOptions = new DbContextOptionsBuilder<ApplicationDbContext>()
-            .UseInMemoryDatabase(Guid.NewGuid().ToString())
+            .UseNpgsql(_postgres.GetConnectionString())
             .ConfigureWarnings(b => b.Ignore(InMemoryEventId.TransactionIgnoredWarning))
             .Options;
         
         Context = new ApplicationDbContext(contextOptions);
-
-        Context.Database.EnsureDeleted();
-        Context.Database.EnsureCreated();
+        
+        await Context.Database.EnsureCreatedAsync();
+    }
+    
+    public Task DisposeAsync()
+    {
+        return _postgres.DisposeAsync().AsTask();
     }
 }
