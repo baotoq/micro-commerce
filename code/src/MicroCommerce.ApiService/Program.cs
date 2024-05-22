5f91d5cfd8df2cb0fc38554541d57bea94b926c5
Bao To <baotoq@outlook.com>
2024-05-22 18:37:56 +0700

    bump nuget version

--- code/src/MicroCommerce.ApiService/Program.cs
+++ code/src/MicroCommerce.ApiService/Program.cs
@@ -58,7 +58,6 @@ app.UseAuthorization();
 app.MapDefaultEndpoints();
 
 app.MapIdentityApi<User>().WithTags("identity");
-app.MapGet("/", () => Results.Redirect("/alive"));
 app.MapCarts();
 app.MapPayments();
 app.MapCategories();
