efd33286cafe507c7bfe52782a2f3805d3207bd4
Bao To <baotoq@outlook.com>
2024-11-15 00:13:08 +0700

    feat: add unit test

---
+++ code/tests/MicroCommerce.Tests/CreateProductValidatorTests.cs
@@ -0,0 +1,25 @@
+using FluentValidation.TestHelper;
+using MicroCommerce.ApiService.Features;
+
+namespace MicroCommerce.Tests;
+
+public class CreateProductValidatorTests
+{
+    private readonly CreateProduct.Validator _validator = new();
+
+    [Fact]
+    public async Task AllError()
+    {
+        // Arrange
+        var request = new CreateProduct.Request
+        {
+            Name = ""
+        };
+
+        // Act
+        var act = await _validator.TestValidateAsync(request);
+
+        // Assert
+        await Verify(act);
+    }
+}
