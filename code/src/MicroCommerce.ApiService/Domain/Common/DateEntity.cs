95c836ca2eb2769d91c8435872f3f59979f5fa97
Bao To Quoc <baotoq@outlook.com>
2024-12-12 20:11:03 +0700

    feat: implement IDateEntity interface for date tracking and update user entity with created and updated timestamps

--- code/src/MicroCommerce.ApiService/Domain/Common/DateEntity.cs
+++ code/src/MicroCommerce.ApiService/Domain/Common/DateEntity.cs
@@ -1,7 +1,13 @@
 namespace MicroCommerce.ApiService.Domain.Common;
 
-public abstract class DateEntity
+public abstract class DateEntity : IDateEntity
 {
     public DateTimeOffset CreatedAt { get; set; } = DateTimeOffset.UtcNow;
+    public DateTimeOffset? UpdatedAt { get; set; }
+}
+
+public interface IDateEntity
+{
+    public DateTimeOffset CreatedAt { get; set; }
     public DateTimeOffset? UpdatedAt { get; set; }
 }
