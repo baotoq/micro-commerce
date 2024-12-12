51ebf84dadf4969c1a4dedffad86261e696cf6b5
Bao To <baotoq@outlook.com>
2024-12-13 02:30:44 +0700

    feat: add reactjs

---
+++ code/src/store/next.config.mjs
@@ -0,0 +1,7 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+  transpilePackages: ["@refinedev/antd"],
+  output: "standalone",
+};
+
+export default nextConfig;
---
+++ code/src/store/src/app/blog-posts/layout.tsx
@@ -0,0 +1,24 @@
+import { Header } from "@components/header";
+import { authProviderServer } from "@providers/auth-provider";
+import { ThemedLayoutV2 } from "@refinedev/antd";
+import { redirect } from "next/navigation";
+import React from "react";
+
+export default async function Layout({ children }: React.PropsWithChildren) {
+  const data = await getData();
+
+  if (!data.authenticated) {
+    return redirect(data?.redirectTo || "/login");
+  }
+
+  return <ThemedLayoutV2 Header={Header}>{children}</ThemedLayoutV2>;
+}
+
+async function getData() {
+  const { authenticated, redirectTo } = await authProviderServer.check();
+
+  return {
+    authenticated,
+    redirectTo,
+  };
+}
