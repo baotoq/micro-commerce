b6cb7b9b1c0085de937d3f9ec523c3ebbecc43cb
Bao To <baotoq@outlook.com>
2026-05-17 20:12:43 +0700

    up

--- src/MicroCommerce.AppHost/AppHost.cs
+++ src/MicroCommerce.AppHost/AppHost.cs
@@ -1,3 +1,5 @@
+#pragma warning disable ASPIREJAVASCRIPT001
+
 var builder = DistributedApplication.CreateBuilder(args);
 
 var cache = builder.AddRedis("cache");
@@ -6,6 +8,11 @@ var server = builder.AddProject<Projects.MicroCommerce_Server>("server")
     .WithReference(cache)
     .WaitFor(cache)
     .WithHttpHealthCheck("/health")
+    .WithExternalHttpEndpoints();
+
+builder.AddNextJsApp("web", "../web")
+    .WithEnvironment("API_URL", server.GetEndpoint("http"))
+    .WaitFor(server)
     .WithExternalHttpEndpoints();
 
 builder.Build().Run();
